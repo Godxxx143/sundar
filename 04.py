7d2b1561c9525c773f86bed87ea385208c8d95b9
Godxxx143 <ssundarajan2006@gmail.com>
2025-07-29 10:22:56 +0530

    Add files via upload

---
+++ 04.py
@@ -0,0 +1,13 @@
+print("Sundar supermarket")
+print("NO.44,nehru street,puducherry.")
+print("**************************************")
+print("Bill recipet")
+print("**************************************")
+sl=input("Enter the serial number:")
+pr=input("Enter the praticulers:")
+rate =int(input("Enter the rate:"))
+qty =int(input("Enter the ouality:"))
+total =rate * qty
+print("Total Amount :",total)
+GST =total * 10 / 100
+print("GST (10%):",GST)
