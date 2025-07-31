a0c4d56d21c16208cd2baea2c857d1f76c201c4d
Godxxx143 <ssundarajan2006@gmail.com>
2025-07-31 14:02:20 +0530

    Add files via upload

---
+++ 01.py
@@ -0,0 +1,9 @@
+print("Arithematic operations")
+print("**********************")
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+print("*result*")
+c =a+b
+print("ADD:",c)
+d =a-b
+print("sub:",d)
